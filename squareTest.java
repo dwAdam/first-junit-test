3e5a11a447dcfb09ff82b241a830c44fce7299bf
dwAdam <adamcooper.w@gmail.com>
2020-05-19 23:07:08 +0200

    Square area test

---
+++ squareTest.java
@@ -0,0 +1,18 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class squareTest {
+
+	@Test
+	public void test() {
+		JunitTesting test = new JunitTesting();
+
+		int output = test.square(5);
+		assertEquals(25, output); // success
+		assertEquals(26, output); // failed
+	}
+
+}
